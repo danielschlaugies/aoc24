7b261bc3c967aff63f6c4a675dd4ed07538cc6c2
danielschlaugies <daniel.schlaugies@live.de>
2024-12-12 22:41:36 +0100

    day05 second half

---
+++ src/bin/day05-second.rs
@@ -0,0 +1,124 @@
+use std::cmp::Ordering;
+use std::fmt;
+use std::io::Read;
+use std::str::FromStr;
+use std::sync::Arc;
+
+#[derive(Eq)]
+struct Page {
+    number: i32,
+    print_order: Arc<Vec<(i32, i32)>>,
+}
+
+impl PartialOrd for Page {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.number == other.number {
+            return Some(Ordering::Equal);
+        }
+
+        for (a, b) in self.print_order.iter() {
+            if *a == self.number && *b == other.number {
+                return Some(Ordering::Less);
+            } else if *a == other.number && *b == self.number {
+                return Some(Ordering::Greater);
+            }
+        }
+
+        None
+    }
+}
+
+impl fmt::Debug for Page {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.number)
+    }
+}
+
+impl PartialEq<Self> for Page {
+    fn eq(&self, other: &Self) -> bool {
+        return self.number == other.number;
+    }
+}
+
+fn in_order(list: &[Page]) -> bool {
+    list.is_sorted_by(|x, y| match x.partial_cmp(y) {
+        Some(Ordering::Greater) => false,
+        _ => true,
+    })
+}
+
+//noinspection DuplicatedCode
+fn main() -> Result<(), ()> {
+    let mut args = std::env::args();
+
+    let s = match args.len() {
+        2 => {
+            //read file
+            let filename = args.nth(1).unwrap();
+            std::fs::read_to_string(filename)
+        }
+        _ => {
+            //read stdin
+            let mut buffer = String::new();
+            match std::io::stdin().read_to_string(&mut buffer) {
+                Ok(_) => Ok(buffer),
+
+                Err(_) => Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "could not read stdin",
+                )),
+            }
+        }
+    };
+
+    match s {
+        Ok(s) => {
+            let mut split = s.split("\n\n");
+            let rule_string = split.next().unwrap();
+            let mut rules: Vec<(i32, i32)> = Vec::new();
+
+            rule_string.lines().for_each(|line| {
+                let mut line_split = line.split("|");
+
+                let a = i32::from_str(line_split.next().unwrap()).unwrap();
+                let b = i32::from_str(line_split.next().unwrap()).unwrap();
+
+                rules.push((a, b));
+            });
+
+            let page_order = Arc::new(rules);
+
+            let mut total = 0;
+
+            let update_string = split.next().unwrap();
+            update_string.lines().for_each(|line| {
+                let mut numbers: Vec<Page> = line
+                    .split(',')
+                    .map(|n| {
+                        let num = n.parse::<i32>().unwrap();
+                        Page {
+                            number: num,
+                            print_order: page_order.clone(),
+                        }
+                    })
+                    .collect();
+
+                if !in_order(numbers.as_slice()) {
+                    numbers.sort_by(|x, y| match x.partial_cmp(y) {
+                        Some(Ordering::Greater) => Ordering::Greater,
+                        _ => Ordering::Less,
+                    });
+
+                    //get mid
+                    let mid = numbers.len() / 2;
+                    total += numbers[mid].number;
+                }
+            });
+
+            println!("{}", total);
+            Ok(())
+        }
+
+        Err(_) => Err(()),
+    }
+}
